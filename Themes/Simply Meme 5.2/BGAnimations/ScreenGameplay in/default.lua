86171832de28961d37d46f8cb16ba602ddb33871
PCBoyGames <patrickwclark12@gmail.com>
2017-07-03 09:50:34 -0500

    Dammit, I didn't do it right!

--- Themes/Simply Meme 5.2/BGAnimations/ScreenGameplay in/default.lua
+++ Themes/Simply Meme 5.2/BGAnimations/ScreenGameplay in/default.lua
@@ -72,7 +72,7 @@ local t = Def.ActorFrame{
 	},
 
 	LoadFont("_wendy small")..{
-		Text=GLHF!,
+		Text=text,
 		InitCommand=cmd(Center; diffusealpha,0; shadowlength,1),
 		OnCommand=cmd(accelerate, 0.5; diffusealpha, 1; sleep, 0.66; accelerate, 0.33; zoom, 10; diffusealpha, 0),
 		CurrentSongChangedMessageCommand=function(self)
